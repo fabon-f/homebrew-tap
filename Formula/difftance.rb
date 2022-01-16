e0f4a571c680b21c02df452f79809a81dbd44a1f
fabon <syobon.hinata.public@gmail.com>
2022-01-16 09:55:27 +0900

    Update difftance

--- Formula/difftance.rb
+++ Formula/difftance.rb
@@ -1,8 +1,8 @@
 class Difftance < Formula
   desc "A diff tool to show edit distance"
   homepage "https://github.com/fabon-f/difftance"
-  url "https://github.com/fabon-f/difftance/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "b33702dd6223e2fab68853d249c637b64cff920ae5c59134a6531f077eb1dc1a"
+  url "https://github.com/fabon-f/difftance/archive/refs/tags/v0.3.0.tar.gz"
+  sha256 "c4080b73c1ff5dc86fea0fa965391af2a65eb8984671c6eced95c88c36aa1b87"
   license "MIT"
 
   depends_on "crystal"
